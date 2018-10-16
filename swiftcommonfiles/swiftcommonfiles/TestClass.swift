8947b3edb8ae2fc8ad840f6fb02bf75fef6bd6f1
Mahesh Asabe <mahesh.asabe@in-it0544.globant.com>
2018-10-16 16:39:22 +0530

    swiftcommonfiles project and podspec created

---
+++ swiftcommonfiles/swiftcommonfiles/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  swiftcommonfiles
+//
+//  Created by Mahesh Asabe on 16/10/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+
+    public class func getName() -> String {
+        return "Mahesh"
+    }
+}
